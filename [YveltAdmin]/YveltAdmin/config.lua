eac232a806c06c25de43c47c47238a71b4951742
Guudz <Kilian1702@outlook.com>
2023-01-29 23:13:08 +0100

    Initial commit

---
+++ [YveltAdmin]/YveltAdmin/config.lua
@@ -0,0 +1,25 @@
+Config = {}
+
+
+Config = {
+    webhook = { -- Vos logs
+        Staffmodeon = "https://discord.com/api/webhooks/992743546576707584/gTn5hiwc98B9xdOsBOrFzB1UIDz-5QT7XAF40xL87EQNfaGU5veV5wI-8stSnuWUsMGB",   
+        Staffmodeoff = "https://discord.com/api/webhooks/992743546576707584/gTn5hiwc98B9xdOsBOrFzB1UIDz-5QT7XAF40xL87EQNfaGU5veV5wI-8stSnuWUsMGB",   
+        teleport = "https://discord.com/api/webhooks/992743546576707584/gTn5hiwc98B9xdOsBOrFzB1UIDz-5QT7XAF40xL87EQNfaGU5veV5wI-8stSnuWUsMGB",
+        teleportTo = "https://discord.com/api/webhooks/992743546576707584/gTn5hiwc98B9xdOsBOrFzB1UIDz-5QT7XAF40xL87EQNfaGU5veV5wI-8stSnuWUsMGB",
+        revive = "https://discord.com/api/webhooks/992743546576707584/gTn5hiwc98B9xdOsBOrFzB1UIDz-5QT7XAF40xL87EQNfaGU5veV5wI-8stSnuWUsMGB",
+        kick = "https://discord.com/api/webhooks/992743546576707584/gTn5hiwc98B9xdOsBOrFzB1UIDz-5QT7XAF40xL87EQNfaGU5veV5wI-8stSnuWUsMGB",
+        SendLogs = "https://discord.com/api/webhooks/992743546576707584/gTn5hiwc98B9xdOsBOrFzB1UIDz-5QT7XAF40xL87EQNfaGU5veV5wI-8stSnuWUsMGB",        
+        report = "https://discord.com/api/webhooks/992743546576707584/gTn5hiwc98B9xdOsBOrFzB1UIDz-5QT7XAF40xL87EQNfaGU5veV5wI-8stSnuWUsMGB",
+        givemoney = "https://discord.com/api/webhooks/992743546576707584/gTn5hiwc98B9xdOsBOrFzB1UIDz-5QT7XAF40xL87EQNfaGU5veV5wI-8stSnuWUsMGB"
+    },
+
+
+
+
+    Touche = { -- Vos touches
+        Noclip = 170, --F3
+        Menu = 57, --F10
+        MenuReport = 344, --F11
+    }
+}
